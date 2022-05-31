49192d2035130e6d8e2e79c8d43f4615beac9f6f
fxxerr <tugberk.muratoglu@gmail.com>
2022-05-31 20:47:40 +0300

    JS-ÖDEV1

---
+++ Odev/Hafta4/Odev1/js/clock.js
@@ -0,0 +1,37 @@
+function showTime() {
+  var date = new Date();
+  var hour = date.getHours();
+  var minute = date.getMinutes();
+  var second = date.getSeconds();
+  var day = date.getDay();
+
+  var days = [
+    "Pazar",
+    "Pazartesi",
+    "Salı",
+    "Çarşamba",
+    "Perşembe",
+    "Cuma",
+    "Cumartesi",
+  ];
+  var dayName = days[day];
+
+  hour = hour < 10 ? "0" + hour : hour;
+  minute = minute < 10 ? "0" + minute : minute;
+  second = second < 10 ? "0" + second : second;
+
+  var time = hour + ":" + minute + ":" + second + " " + dayName;
+  document.getElementById("myClock").innerHTML = time;
+
+  setTimeout(showTime, 1000);
+}
+
+let name = prompt("İsminizi Giriniz");
+
+let isim = document.querySelector("#myName");
+isim.innerHTML = `${name}`;
+
+let saat = document.querySelector("#myClock");
+saat.innerHTML = showTime();
+
+showTime();
